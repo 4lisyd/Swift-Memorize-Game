c3487fd623fc35e5d060c1417d5703ce3b3dc782
Ali Syed <4limsyed@gmail.com>
2021-10-24 15:15:23 +0500

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by ali on 10/24/21.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
